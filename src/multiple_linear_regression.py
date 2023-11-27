04f9bb87ec50c900fa9eae6517c6ae3f5e406f34
samkramer08 <82391611+samkramer08@users.noreply.github.com>
2023-11-27 11:20:23 +0100

    Update multiple_linear_regression.py

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+class MultipleLinearRegression:
+    def __init__(self, default_intercept:float = 0, default_slope:float= 0):
+        self._intercept = default_intercept
+        self._slope = default_slope
+
+    def train(self,X, y):
+        
+
+    def predict(self, X):
+       
+
+        return predictions
+
+if __name__ == "__main__":
