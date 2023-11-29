e642b9e3559ac643ce067ad06670848f348ac0bd
setosetoseto <151162521+setosetoseto@users.noreply.github.com>
2023-11-29 16:25:28 +0100

    Update multiple_linear_regression.py
    still needs lot of work

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -6,11 +6,28 @@ class MultipleLinearRegression:
         self._slope = default_slope
 
     def train(self,X, y):
-        
+        Xmean = np.mean(X, axis=0)
+        Ymean = np.mean(y)
+
+        # using closed-form solution 
+        p = 
+        n = 
+
+        self._slopes = np.linalg.inv(X.T @ X) @ X.T @ y
+        self._intercept = Ymean - np.sum(Xmean * self.slopes)
+
+        #account for intercept (add row of 1's)
+        X_matrix_with_ones = np.column_stack((np.ones(X.shape[0]), X))
+
+        self.w = np.linalg.inv(X_matrix_with_ones.T @ X_matrix_with_ones) @ X_matrix_with_ones.T @ y
 
     def predict(self, X):
-       
-
+        predictions = X_matrix_with_ones @ self.w
         return predictions
+
+
+
+   
+
 
 if __name__ == "__main__":
