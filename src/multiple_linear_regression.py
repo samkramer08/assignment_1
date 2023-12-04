ba38bebde2abbfd9733153d6626df92f41a8b076
samkramer08 <82391611+samkramer08@users.noreply.github.com>
2023-12-04 12:41:49 +0100

    Update multiple_linear_regression.py

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -24,26 +24,21 @@ if __name__ == "__main__":
     X_train = np.random.rand(100, 3)
     y_train = 2 * X_train[:, 0] + 3 * X_train[:, 1] - 5 * X_train[:, 2] + np.random.randn(100)
 
-    # Training our own model
+
     our_model = MultipleLinearRegression()
     our_model.train(X_train, y_train)
 
-    # Train scikit-learn model
     sklearn_model = LinearRegression()
     sklearn_model.fit(X_train, y_train)
 
-    # Generate some test data
     X_test = np.random.rand(10, 3)
-
-    # Predictions made with our model
+    
     our_model_predictions = our_model.predict(X_test)
 
-    # Predictions made with scikit-learn model
+
     sklearn_predictions = sklearn_model.predict(X_test)
 
-    # Compare predictions
     print("Our Model Predictions:", our_model_predictions)
     print("Scikit-learn Model Predictions:", sklearn_predictions)
 
-    # Check if the predictions are close
     assert np.allclose(our_model_predictions, sklearn_predictions), "Predictions do not match!"
