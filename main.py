bc4e70a9198f6a3e08977437a8b28293cd0c5249
setosetoseto <151162521+setosetoseto@users.noreply.github.com>
2023-12-04 12:35:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from src.multiple_linear_regression import MultipleLinearRegression
 import numpy as np
 from sklearn import datasets
+
+if __name__ == "__main__":
+  model = MultipleLinearRegression()
+
+  
+  
