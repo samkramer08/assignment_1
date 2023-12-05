3c0bee430ad4f094bb09b74d5d0268f1a6c48635
setosetoseto <151162521+setosetoseto@users.noreply.github.com>
2023-12-05 17:56:27 +0100

    Update model_saver.py

--- src/model_saver.py
+++ src/model_saver.py
@@ -1,1 +1,30 @@
+import pickle
+import json
+
+class ModelSaver:
+    def __init__(self, format_type='pickle'): # default format (pickle)
+        self.format_type = format_type
+
+    def save(self, model, filename): # dont know what the filename is supposed to be 
+        if self.format_type == 'pickle':
+            with open(filename, 'wb') as file:
+                pickle.dump(model.get_params(), file)
+        elif self.format_type == 'json':
+            with open(filename, 'w') as file:
+                json.dump(model.get_params(), file)
+        else:
+            pass
+
+    def load(self, model, filename):
+        if self.format_type == 'pickle':
+            with open(filename, 'rb') as file:
+                parameters = pickle.load(file)
+        elif self.format_type == 'json':
+            with open(filename, 'r') as file:
+                parameters = json.load(file)
+        else:
+            pass
+
+
+
 
