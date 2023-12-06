0051f257bb0c1ecd66e7f63e8314f79feaa12c00
setosetoseto <151162521+setosetoseto@users.noreply.github.com>
2023-12-06 16:58:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from src.multiple_linear_regression import MultipleLinearRegression
 from src.model_saver import ModelSaver
-from regression_plotter import RegressionPlotter
-import numpy as np
-from sklearn import datasets
+from sklearn.datasets import load_diabetes
+from src.regressrion_plotter import regressrion_plotter
 
 if __name__ == "__main__":
-  
-  diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+	model = MultipleLinearRegression()
+	ms = model_saver(format_type='csv')
+	ms.save_params(model, 'diabetet_dataset.csv')
+	ms.load_params(model, 'diabetes_dataset.csv')
 
-   model = MultipleLinearRegression()
+	data = sklearn.datasets.load_diabetes(return_X_y=True)
+	x, y = data
 
-  
+	#split data into training and testing data
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-  
+	# train model
+	model.fit(x_train, y_train)
+
+	#make prediction
+	y_pred = model.predict(x_test)
+
+	# mean squared error to compare predictions to grownd truth
+	meanSqError = mean_squared_error(y_test, y_pred)
+
+  # using regression plotter to visualyze results
+	plotter = RegressionPlotter()
+	plotter.set_data({'feature': x_test[:, 0], 'target': y_test}) 
+	plotter.plot(model, features=['feature'])
 
 
-  
-  
