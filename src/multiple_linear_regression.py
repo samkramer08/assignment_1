e75ee5805b0c1d577b2fd9a38b8a0897eace8b2a
setosetoseto <151162521+setosetoseto@users.noreply.github.com>
2023-12-06 21:23:59 +0100

    Update multiple_linear_regression.py

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -10,40 +10,47 @@ class MultipleLinearRegression:
         Xmean = np.mean(X, axis=0)
         Ymean = np.mean(y)
 
+        # equation to find slope and intercept (b)
         self._slopes = np.linalg.inv(X.T @ X) @ X.T @ y
         self._intercept = Ymean - np.sum(Xmean * self._slopes)
 
     def predict(self, X):
         # Add a column of ones to X matrix for the intercept term
         X_matrix_with_ones = np.column_stack((np.ones(X.shape[0]), X))
-        predictions = X_matrix_with_ones @ np.insert(self._slopes, 0, self._intercept)
+        
+       # Insert intercept at the beginning of the slopes array
+        slopes = np.insert(self._slopes, 0, self._intercept)
+
+        # find predictions by multiplying matrix with ones slopes
+        predictions = X_matrix_with_ones @ slopes
         return predictions
 
 if __name__ == "__main__":
+    # generate random training data
     np.random.seed(42)
-    X_train = np.random.rand(100, 3)
-    y_train = 2 * X_train[:, 0] + 3 * X_train[:, 1] - 5 * X_train[:, 2] + np.random.randn(100)
+    x_train = np.random.rand(100, 3)
+    y_train = 2 * x_train[:, 0] + 3 * x_train[:, 1] - 5 * x_train[:, 2] + np.random.randn(100)
 
     # Training our own model
-    our_model = MultipleLinearRegression()
-    our_model.train(X_train, y_train)
+    MLR = MultipleLinearRegression()
+    MLR.train(x_train, y_train)
 
     # Train scikit-learn model
-    sklearn_model = LinearRegression()
-    sklearn_model.fit(X_train, y_train)
+    SKL = LinearRegression()
+    SKL.fit(x_train, y_train)
 
-    # Generate some test data
-    X_test = np.random.rand(10, 3)
+    # Generate random testing data
+    x_test = np.random.rand(10, 3)
 
     # Predictions made with our model
-    our_model_predictions = our_model.predict(X_test)
+    MLR_predictions = MLR.predict(x_test)
 
     # Predictions made with scikit-learn model
-    sklearn_predictions = sklearn_model.predict(X_test)
+    SKL_predictions = SKL.predict(x_test)
 
     # Compare predictions
-    print("Our Model Predictions:", our_model_predictions)
-    print("Scikit-learn Model Predictions:", sklearn_predictions)
+    print("Our Model Predictions:", MLR_predictions)
+    print("Scikit-learn Model Predictions:", SKL_predictions)
 
 
 
