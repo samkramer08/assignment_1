457ebeeb1528351d170fbda63b2527a3edb1828c
setosetoseto <151162521+setosetoseto@users.noreply.github.com>
2023-12-06 17:39:22 +0100

    Update regression_plotter.py

--- src/regression_plotter.py
+++ src/regression_plotter.py
@@ -5,55 +5,55 @@ class RegressionPlotter:
     def __init__(self, data):
         self.data = data
 
-    def plotter(self, target, features):
+    def plotter(self, target, features=None, model=None):
+    if features is None:
         features = list(self.data.columns)
-        features.remove(target)
-        num_features = len(features)
+        features.remove(target)  # Remove the target column
 
-        if num_features == 1:
-            x = self.data[features[0]]
-            y = model.predict(self.data)
+    num_features = len(features)
 
-            plt.scatter(x, self.data[target], label='Data')
-            plt.plot(x, y, color='red', label='Regression')
-            plt.xlabel('Feature')
-            plt.ylabel('Target')
-            plt.legend()
-            plt.show()
-            
-        elif num_features == 2:
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
 
-            x = self.data[features[0]]
-            y = self.data[features[1]]
-            z = model.predict(self.data)
+    if num_features == 1:
+        x = self.data[features[0]]
+        y = model.predict(x)
 
-            ax.scatter(x, y, self.data[target], label='Data')
-            ax.plot_trisurf(x, y, z, color='red', alpha=0.5, label='Regression')
-            ax.set_xlabel(features[0])
-            ax.set_ylabel(features[1])
-            ax.set_zlabel(target)
-            ax.legend()
-            plt.show()
+        plt.scatter(x, self.data[target], label='Data')
+        plt.plot(x, y, color='red', label='Regression')
+        plt.xlabel('Feature')
+        plt.ylabel('Target')
+        plt.legend()
+        plt.show()
 
-        else: 
-            fig, axes = plt.subplots(num_features, 1, figsize=(8, 4 * num_features), sharey=True)
+     elif num_features == 2:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
 
-            for i, feature in enumerate(features):
-                x = self.data[feature]
-                y = model.predict(self.data)
+        x = self.data[features[0]]
+        y = self.data[features[1]]
+        z = model.predict(self.data)
 
-                axes[i].scatter(x, self.data[target], label='Actual data')
-                axes[i].plot(x, y, color='red', label='Regression')
-                axes[i].set_xlabel(feature)
-                axes[i].set_ylabel(target)
-                axes[i].legend()
+        ax.scatter(x, y, self.data[target], label='Data')
+        ax.plot_trisurf(x, y, z, color='red', alpha=0.5, label='Regression')
+        ax.set_xlabel(features[0])
+        ax.set_ylabel(features[1])
+        ax.set_zlabel(target)
+        ax.legend()
+        plt.show()
 
-            plt.tight_layout()
-            plt.show()
+    else:
+        fig, axes = plt.subplots(num_features, 1, figsize=(8, 4 * num_features), sharey=True)
 
-if __name__ == "__main__":
-# dont knwo what do do here :)
+        for i, feature in enumerate(features):
+            x = self.data[feature]
+            y = model.predict(x)
+
+            axes[i].scatter(x, self.data[target], label='Actual data')
+            axes[i].plot(x, y, color='red', label='Regression')
+            axes[i].set_xlabel(feature)
+            axes[i].set_ylabel(target)
+            axes[i].legend()
+
+        plt.tight_layout()
+        plt.show()
 
 
